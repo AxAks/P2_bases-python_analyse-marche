282cd441d0944037b0f7cac5b2d7d171806c5bee
Aksel <axel.akonde@gmail.com>
2020-12-22 00:02:28 +0100

    on recupere bien toutes les informations demandées pour une page produit

--- scrape_book.py
+++ scrape_book.py
@@ -4,11 +4,10 @@ from lxml import html
 from bs4 import BeautifulSoup as bs
 import csv
 
-
 url = 'http://books.toscrape.com/catalogue/holidays-on-ice_167/index.html'
 fichier = open("./surveillance_prix.csv", "w+")
 
-resultat = {}
+
 """
 clés du dictionnaire (10):
 product_page_url
@@ -24,56 +23,41 @@ image_url
 """
 
 """ récupération des informations du livre dans un dictionnaire """
-def get_book_infos(url):
-    path_universal_product_code = ''
-    path_title = '/html/body/div/div/div[2]/div[2]/article/div[1]/div[2]/h1'  # récupéré via inspect/copy/Xpath
-    path_price_including_tax = ''
-    path_price_excluding_tax = ''
-    path_number_available = ''
-    path_product_descr = '/html/body/div/div/div[2]/div[2]/article/p'
-    path_category = ''
-    path_review_rating = ''
 
+
+def get_book_infos(url):
+    resultat = {}
     response = requests.get(url)  # verifie le code statut de la requete si 200 tout est OK !
     byte_data = response.content  # le contenu brut de la page
     soup = bs(byte_data, 'lxml')
     title = soup.find_all('h1')[0].get_text()
     product_descr = soup.find_all('p')[3].get_text()
-    print(title)
-    print(product_descr)
-    table_rows = soup.find_all('tr')
-    table_data = soup.find_all('td')[0].get_text()
-    print('---')
-    print(table_rows)
-    print('---')
-    print(table_data)
+    upc = soup.find_all('td')[0].get_text()
+    category = soup.find_all('td')[1].get_text()
+    price_no_tax = soup.find_all('td')[2].get_text()
+    price_with_tax = soup.find_all('td')[3].get_text()
+    availability = soup.find_all('td')[5].get_text()
+    reviews_rating = soup.find_all('p', class_='star-rating')[0].get('class')[1]
     image_url = soup.find('img')['src']
-    print(image_url)
+    print(f"URL : {url}")
+    print(f"UPC : {upc}")
+    print(f"Titre : {title}")
+    print(f"Prix sans TVA :{price_no_tax}")
+    print(f"Prix avec TVA : {price_with_tax}")
+    print(f"Disponibilité : {availability}")
+    print(f"Résumé : {product_descr}")
+    print(f"Categorie : {category}")
+    print(f"Note : {reviews_rating}")
+    print(f"URL de l'image : {image_url}")
     """
-    #  source_code = html.fromstring(byte_data)
-    #  title = source_code.xpath(path_title)
-    # product_descr = source_code.xpath(path_product_descr)
-    
-     # beautiful soup, recupération du contenu de la première balise image
-
-    title_string = title[0].text_content()  # stockage dans une variable
-    image_url_string = image_url['src']  # idem
-    product_descr_string = product_descr[0].text_content() #idem
-    return url, title_string, image_url_string, product_descr_string  # resultat de la fonction
+    return url, upc, title, price_no_tax, price_with_tax, availability,\
+           product_descr, category, reviews_rating, image_url  # resultat de la fonction
     """
 
 
-# à enlever !! on va utiliser que beautiful soup pour parser et recuperer
-""" cette fonction ne récupère que les tableaux infos supplémentaires """
-def get_table_one_product(url):
-    print("Infos supplémentaires : ")
-    product_info = pd.read_html(url)
-    # print(len(product_info))
-    print(product_info)
-
-
+"""
 #  pandas pour gerer tout le csv
-""" ecrire les resultats dans le fichier csv """
+ecrire les resultats dans le fichier csv
 def write_csv(fichier):
     fichier.file.seek(0, 2)
 
@@ -88,5 +72,5 @@ def main():
 
 if __name__ == "main":
     main()
-
+"""
 get_book_infos(url)
